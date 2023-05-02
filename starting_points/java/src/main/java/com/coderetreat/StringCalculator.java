728f1902fd975b35f19adde45e6637343fa6c7d0
gomrisev <gomrisev@students.zhaw.ch>
2023-05-02 19:32:07 +0200

    Test7 successful

--- starting_points/java/src/main/java/com/coderetreat/StringCalculator.java
+++ starting_points/java/src/main/java/com/coderetreat/StringCalculator.java
@@ -35,4 +35,13 @@ public class StringCalculator {
         // Standard
         return 0;
     }
+
+    public int Multiply(String numbers) {
+        if(numbers == "") {
+            return 0;
+        }
+
+        // Standard
+        return 0;
+    }
 }
